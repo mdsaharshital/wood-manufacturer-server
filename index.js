0d41d332cd5162ccf5853353034db334e622ffcf
mdsaharshital <mdsaharshitol@gmail.com>
2022-05-22 13:39:11 +0600

    update for delivery and set new quantity

--- index.js
+++ index.js
@@ -23,11 +23,31 @@ const client = new MongoClient(uri, {
 async function run() {
   await client.connect();
   const productCollection = client.db("fortunio_timber").collection("products");
+  console.log("listening from DB");
   try {
+    // update a product delivery
+    app.post("/product/:id", async (req, res) => {
+      const id = req.params;
+      const available_quantity = req.body.newQuantity;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = { $set: { available_quantity } };
+      const results = await productCollection.updateOne(filter, updatedDoc);
+      if (results?.acknowledged) {
+        res.send({ success: true, message: "Product Booked" });
+      }
+      console.log(available_quantity);
+    });
     // get all products-----------
     app.get("/products", async (req, res) => {
       const query = {};
       const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+    // get products through id
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params;
+      const filter = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(filter);
       res.send(result);
     });
   } finally {
